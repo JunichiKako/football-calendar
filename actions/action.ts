0fbe7bce06ab4efcf6417a246d42bdffe1a11e66
JunichiKako <cjaucono@gmail.com>
2024-07-24 22:47:56 +0900

    add:clerkとsupabaseの連携

--- actions/action.ts
+++ actions/action.ts
@@ -1,13 +1,25 @@
 "use server";
 
 import { createClerkSupabaseClient } from "@/lib/supabase/clerk";
+import { currentUser } from "@clerk/nextjs/server";
 
 export const createProfile = async (name: string) => {
   const supabase = await createClerkSupabaseClient();
 
+  const user = await currentUser();
+
+  if(!user){
+    throw new Error('ログインしてください')
+  }
+
+  const user_id = user.id
+
   const { error } = await supabase.from("users").insert([
     {
       name,
+      user_id
     },
   ]);
+  console.log();
+  
 };
