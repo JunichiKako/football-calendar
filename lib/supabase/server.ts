6b1fbb396faba1be7cc05ae513e7f38ee5de3882
Junichi Kako <cjaucono@gmail.com>
2024-12-20 19:36:53 +0900

    データベースの型生成のコマンド追加

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,17 +1,17 @@
-import { Database } from '@/types/database';
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
-import { cookies } from 'next/headers';
- 
+import { Database } from "@/types/database";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
 // Define a function to create a Supabase client for server-side operations
 // The function takes a cookie store created with next/headers cookies as an argument
 export const createClient = () => {
   const cookieStore = cookies();
- 
+
   return createServerClient<Database>(
     // Pass Supabase URL and anonymous key from the environment to the client
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
- 
+
     // Define a cookies object with methods for interacting with the cookie store and pass it to the client
     {
       cookies: {
@@ -31,7 +31,7 @@ export const createClient = () => {
         // The remove method is used to delete a cookie by its name
         remove(name: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value: '', ...options });
+            cookieStore.set({ name, value: "", ...options });
           } catch (error) {
             // If the remove method is called from a Server Component, an error may occur
             // This can be ignored if there is middleware refreshing user sessions
