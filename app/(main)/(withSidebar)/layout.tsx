384b3c6de1d4987543ee36872e58252fe74e0b73
Junichi Kako <cjaucono@gmail.com>
2025-01-27 08:17:19 +0900

    コンポーネントの誤字の修正

--- app/(main)/(withSidebar)/layout.tsx
+++ app/(main)/(withSidebar)/layout.tsx
@@ -1,4 +1,4 @@
-import ViewController from '@/components/main/view-contriller';
+import ViewController from '@/components/main/view-controller';
 import Sidebar from '@/components/sidebar/sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
