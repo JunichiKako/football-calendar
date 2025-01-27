2ed4e4d6784aca9cffeb5a036eee6e817323dafc
Junichi Kako <cjaucono@gmail.com>
2025-01-27 23:41:31 +0900

    view=calendarでFooterを表示させないように調整

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,10 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className='text-sm flex space-x-4 text-muted-foreground cursor-pointer'>
+    <footer
+      className='text-sm flex space-x-4 text-muted-foreground cursor-pointer'
+      data-calendar-hide
+    >
       <Link href='privacy-policy'>プライバシーポリシー</Link>
       <Link href='terms'>利用規約</Link>
       <Link href='legal-information'>特定商取引法に基づく表記</Link>
--- components/main/view-controller.tsx
+++ components/main/view-controller.tsx
@@ -3,11 +3,14 @@
 import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
+// view が calendar の場合はサイドバーとFooterを非表示にする
 export default function ViewController() {
   const searchParams = useSearchParams();
   const view = searchParams.get('view');
 
   useEffect(() => {
+    // htmlタグのstyleに --sidebar-display というカスタムプロパティをview=calendarの場合、追加
+    // データ属性を追加することで、CSSをつけて表示非表示を切り替えてます
     const root = document.documentElement;
     if (view === 'calendar') {
       root.style.setProperty('--sidebar-display', 'none');
