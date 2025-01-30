4ada88c0ed63c457cd36a3475176a58a608751f7
Junichi Kako <cjaucono@gmail.com>
2025-01-30 18:40:05 +0900

    leagueデータ取得のコメント書き

--- data/league.ts
+++ data/league.ts
@@ -8,8 +8,10 @@ import getDateRange from '@/utils/getDate';
 
 // 基本となるリーグデータを取得
 const fetchLeagueData = cache(async () => {
+  // APIの取得範囲の制御
   const { dateFrom, dateTo } = getDateRange();
 
+  // リーグIDごとにAPIを取得
   const leagues = await Promise.all(
     leagueIds.map(async (id) => {
       try {
@@ -32,6 +34,7 @@ const fetchLeagueData = cache(async () => {
 
         const data: league = await res.json();
 
+        // matchesと配列じゃない場合のエラー処理
         if (!data.matches || !Array.isArray(data.matches)) {
           console.error(
             `Unexpected response format for league ${id}: matches property is missing or not an array`
@@ -40,6 +43,7 @@ const fetchLeagueData = cache(async () => {
         }
 
         return data.matches.map((match) => {
+          // 日付と時間の最適化処理
           const matchDateTime = new Date(match.utcDate);
           const matchDate = matchDateTime.toLocaleDateString('ja-JP', {
             month: 'numeric',
@@ -50,16 +54,20 @@ const fetchLeagueData = cache(async () => {
             hour: '2-digit',
             minute: '2-digit',
           });
+
+          // シーズンの開始年と終了年を取得
           const seasonStartYear = new Date(
             match.season.startDate
           ).getFullYear();
           const seasonEndYear = new Date(match.season.endDate).getFullYear();
 
+          // チーム名の翻訳処理
           const homeTeam =
             teamTranslations[match.homeTeam.name] || match.homeTeam.name;
           const awayTeam =
             teamTranslations[match.awayTeam.name] || match.awayTeam.name;
 
+          // 必要なデータを返す
           return {
             seasonStartYear,
             seasonEndYear,
@@ -82,8 +90,9 @@ const fetchLeagueData = cache(async () => {
       }
     })
   );
-
+  // 2次元配列を1次元に変換
   const allLeagues = leagues.flat();
+  // 日時順にソートして返す
   return sortMatchesByDateTime(allLeagues);
 });
 
@@ -98,6 +107,7 @@ const sortMatchesByDateTime = (matches: Match[]): Match[] => {
 
 // リーグごとの表示に必要なデータを取得
 export const getLeagueByGroup = cache(async () => {
+  // キャッシュを利用してデータを取得
   const matches = await fetchLeagueData();
 
   const grouped = matches.reduce((acc, match) => {
@@ -114,6 +124,7 @@ export const getLeagueByGroup = cache(async () => {
     return acc;
   }, {} as Record<string, { leagueId: number; leagueName: string; leagueImg: string; matches: Match[] }>);
 
+  // リーグIDの順番にソート idが小さいものから順に並べる
   const orderedGrouped = Object.fromEntries(
     Object.entries(grouped).sort(
       (a, b) =>
@@ -131,6 +142,7 @@ export const getLeagueByGroup = cache(async () => {
 // 時間順に試合を取得する関数
 export const getLeagueMatchesByTime = cache(
   async (selectedLeagues: string[] = []) => {
+    // sortedMatchesを利用して時系列のデータを取得
     const matches = await fetchLeagueData();
     return selectedLeagues.length > 0
       ? matches.filter((match) => selectedLeagues.includes(match.leagueName))
