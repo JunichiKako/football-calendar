a40b3de3198e2a9449a4de41aa0861e0832d5bf8
Junichi Kako <cjaucono@gmail.com>
2025-02-13 20:46:26 +0900

    next15の変更

--- config.ts
+++ config.ts
@@ -1,1 +1,5 @@
-export const APP_NAME = "Football Table";
+export const APP_NAME = 'Football Table';
+
+export const runtime = 'edge'; // または 'nodejs'
+export const preferredRegion = 'auto';
+export const dynamic = 'force-static';
