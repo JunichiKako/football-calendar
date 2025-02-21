6b3be4b9d78e0da34f21730416deaf778213015b
Junichi Kako <cjaucono@gmail.com>
2025-02-21 17:40:47 +0900

    Vercel analyticsの導入

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/header';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       <div className='h-[calc(100dvh-56px)]'>{children}</div>
+      <Analytics />
     </>
   );
 }
