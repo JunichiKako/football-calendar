88a11998028a94d40e36aa9528a8de7557cb30d0
Junichi Kako <cjaucono@gmail.com>
2025-02-22 09:16:55 +0900

    もう一度authのパスの変更

--- actions/auth.ts
+++ actions/auth.ts
@@ -15,7 +15,7 @@ export const signInWithGoogle = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback`,
       scopes: [
         'email',
         'profile',
