9ac62674de74fc194c26f68a6228a3bfed1a71c7
Junichi Kako <cjaucono@gmail.com>
2025-02-22 11:34:17 +0900

    useoptimisticの導入

--- components/main/match-check-box.tsx
+++ components/main/match-check-box.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useOptimistic } from 'react';
 
 type MatchCheckboxProps = {
   matchId: number;
@@ -15,36 +16,38 @@ export default function MatchCheckbox({
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // 楽観的な更新のための状態管理
+  const [optimisticChecked, addOptimisticCheck] = useOptimistic(
+    isSelected,
+    (state, newValue: boolean) => newValue
+  );
 
-    // 現状のparamsを渡して、新しいparamsを作成
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 楽観的に更新
+    addOptimisticCheck(e.target.checked);
+
+    // URLパラメータの更新
     const params = new URLSearchParams(searchParams);
-    
-    // 現在選択されているものを取得 filter(Boolean)で空文字を除外
     const currentSelected =
       params.get('selectedMatches')?.split(',').filter(Boolean) || [];
 
-    // チェックボックスがチェックされている場合、選択されたマッチのIDをSearchParamsに追加
     if (e.target.checked) {
       if (!currentSelected.includes(matchId.toString())) {
         currentSelected.push(matchId.toString());
       }
     } else {
-      // チェックボックスがチェックされていない場合、選択されたマッチのIDをSearchParamsから削除
       const index = currentSelected.indexOf(matchId.toString());
       if (index > -1) {
         currentSelected.splice(index, 1);
       }
     }
 
-    // 元々のSearchParamsにselectedMatchesがあれば、新しいselectedMatchesを追加
     if (currentSelected.length > 0) {
       params.set('selectedMatches', currentSelected.join(','));
     } else {
       params.delete('selectedMatches');
     }
 
-    // スクロール位置を維持するオプションを追加 
     router.replace(`${pathname}?${params.toString()}`, {
       scroll: false,
     });
@@ -57,7 +60,7 @@ export default function MatchCheckbox({
       value={matchId.toString()}
       id={matchId.toString()}
       className='peer hidden'
-      checked={isSelected}
+      checked={optimisticChecked}
       onChange={handleChange}
     />
   );
