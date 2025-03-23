efe35a5ea2d8759e8eb25419542ecc8a58042901
Junichi Kako <cjaucono@gmail.com>
2025-03-23 13:11:33 +0900

    リーグのキャッシュを12時間に

--- data/league.ts
+++ data/league.ts
@@ -104,7 +104,7 @@ const fetchLeagueData = unstable_cache(
   },
   ['league-data'],
   {
-    revalidate: 3600,
+    revalidate: 43200,
     tags: ['leagues'],
   }
 );
@@ -225,7 +225,7 @@ export const getLeagueByGroup = unstable_cache(
   },
   ['league-groups'],
   {
-    revalidate: 3600,
+    revalidate: 43200,
     tags: ['group-leagues'],
   }
 );
@@ -241,7 +241,7 @@ export const getLeagueMatchesByTime = unstable_cache(
   },
   ['league-matches-time'],
   {
-    revalidate: 3600,
+    revalidate: 43200,
     tags: ['time-leagues'],
   }
 );
